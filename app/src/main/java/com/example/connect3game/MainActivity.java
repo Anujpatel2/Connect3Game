3d0fcc311e72943cddaa2637ac83ab2400e73e77
Anuj Patel <anujmaitri@gmail.com>
2020-05-12 23:46:08 -0400

    Initial Working Product

---
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.connect3game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 0:Yellow Player
+    // 1:Red Player
+    int activePlayer =0;
+    boolean gameActive = true;
+    int [][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {2,4,6}, {0,4,8}};
+
+    //2 indicates no counter in the slot
+    int [] gameState = {2,2,2,2,2,2,2,2,2};
+
+    public void dropIn(View view){
+        android.widget.ImageView counter = (ImageView) view;
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter]==2 && gameActive) {
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    //someone has one
+                    gameActive=false;
+                    String winner = "";
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    android.widget.Toast.makeText(this, winner + " has won!", android.widget.Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
